2c764771999a34cf81910d3e440ae7cbb643fb5d
Chaudhry Junaid Anwar <junaidanwar10@gmail.com>
2019-01-23 19:43:22 +0500

    add vanilla implementation using AsyncStorage + add peer dependencies

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
-// placeholder
+import { AsyncStorage } from 'react-native';
+import * as Keychain from 'react-native-keychain';
+
+export default ({keyName = 'redux-persist-encrypted-async-storage:key'} = {}) => {
+  const noop = () => null;
+
+  return {
+    async getItem(key, callback = noop) {
+      try {
+        let result = await AsyncStorage.getItem(key);
+
+        callback(null, result);
+        return result;
+      } catch (error) {
+        callback(error);
+        throw error;
+      }
+    },
+
+    async setItem(key, value, callback = noop) {
+      try {
+        await AsyncStorage.setItem(key, value);
+        callback(null);
+      } catch (error) {
+        callback(error);
+        throw error;
+      }
+    },
+
+    async removeItem(key, callback = noop) {
+      try {
+        await AsyncStorage.removeItem(key);
+        callback(null);
+      } catch (error) {
+        callback(error);
+        throw error;
+      }
+    },
+
+    async getAllKeys(callback = noop) {
+      try {
+        const result = await AsyncStorage.getAllItems();
+
+        callback(null, result);
+        return result;
+      } catch (error) {
+        callback(error);
+        throw error;
+      }
+    },
+  };
+};
